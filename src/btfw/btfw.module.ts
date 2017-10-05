454ec369de7d6ef5bada266ca2f592fc8e4e9491
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2017-10-05 12:22:16 -0400

    up

--- src/btfw/btfw.module.ts
+++ src/btfw/btfw.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
+import { ContentComponent } from './content/content.component'
+import { TitleBarComponent } from './title-bar/title-bar.component'
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    FrameworkBodyComponent
+    FrameworkBodyComponent,
+    ContentComponent,
+    TitleBarComponent
   ],
   exports: [
     FrameworkBodyComponent
