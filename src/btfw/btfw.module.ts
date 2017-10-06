dfe5e669393e1fc4eff1e71e60f4567613f5c5e7
mahikero <federicoparassarmiento@yahoo.com>
2017-10-06 16:58:40 -0400

    up

--- src/btfw/btfw.module.ts
+++ src/btfw/btfw.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 import { ContentComponent } from './content/content.component'
 import { TitleBarComponent } from './title-bar/title-bar.component'
+import { TopBarComponent } from './top-bar/top-bar.component'
+import { StatusBarComponent } from './status-bar/status-bar.component';
 
 import { FrameworkConfigService } from '../btfw/services/framework-config.service';
 
@@ -14,7 +16,8 @@ import { FrameworkConfigService } from '../btfw/services/framework-config.servic
   declarations: [
     FrameworkBodyComponent,
     ContentComponent,
-    TitleBarComponent
+    TitleBarComponent,
+    TopBarComponent
   ],
   providers: [
     FrameworkConfigService
--- src/btfw/top-bar/top-bar.component.ts
+++ src/btfw/top-bar/top-bar.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FrameworkConfigService } from '../services/framework-config.service';
+
 @Component({
-  selector: 'top-bar',
+  selector: 'btfw-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private frameworkConfigService: FrameworkConfigService) { }
 
   ngOnInit() {
   }
