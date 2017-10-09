7a8123b8639c27b24b78a96efc10dcaa84d37265
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2017-10-09 15:14:42 -0400

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { BtfwModule } from '../btfw/Btfw.module';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { CountriesComponent } from './countries/countries.component';
 import { SettingsComponent } from './settings/settings.component';
 import { appRoutes } from './app.routing';
+import { CountryDetailComponent } from './country-detail/country-detail.component';
+import { CountryListComponent } from './country-list/country-list.component';
+import { CountryMaintComponent } from './country-maint/country-maint.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    CountriesComponent,
-    SettingsComponent
+    SettingsComponent,
+    CountryDetailComponent,
+    CountryListComponent,
+    CountryMaintComponent
   ],
   imports: [
     BrowserModule,
--- src/btfw/btfw.module.ts
+++ src/btfw/btfw.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
 
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
-import { ContentComponent } from './content/content.component'
-import { TitleBarComponent } from './title-bar/title-bar.component'
-import { TopBarComponent } from './top-bar/top-bar.component'
+import { ContentComponent } from './content/content.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 import { StatusBarComponent } from './status-bar/status-bar.component';
-import { ScreenBelowLarge } from './directives/screen-below-large.directive'
-import { ScreenLarge } from './directives/screen-large.directive'
+import { ScreenBelowLarge } from './directives/screen-below-large.directive';
+import { ScreenLarge } from './directives/screen-large.directive';
 import { MenuComponent } from './menus/menu/menu.component';
-import { MenuItemComponent } from'./menus/menu-item/menu-item.component';
+import { MenuItemComponent } from './menus/menu-item/menu-item.component';
+import { PopupMenuComponent } from './menus/popup-menu/popup-menu.component';
 
 import { FrameworkConfigService } from '../btfw/services/framework-config.service';
 import { ScreenService } from '../btfw/services/screen.service';
@@ -30,7 +31,8 @@ import { MenuService } from '../btfw/services/menu.service';
     ScreenBelowLarge,
     ScreenLarge,
     MenuComponent,
-    MenuItemComponent
+    MenuItemComponent,
+    PopupMenuComponent
   ],
   providers: [
     FrameworkConfigService,
