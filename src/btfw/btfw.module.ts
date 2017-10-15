1d0a470551e40ba5bd186b9fe8b7509694535ef4
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2017-10-15 04:49:44 -0400

    up

--- src/btfw/btfw.module.ts
+++ src/btfw/btfw.module.ts
@@ -20,6 +20,7 @@ import { SignInComponent } from './users/sign-in/sign-in.component';
 import { RegisterUserComponent } from './users/register-user/register-user.component';
 import { DynamicFormComponent } from './dynamic-forms/dynamic-form/dynamic-form.component';
 import { DynamicFieldComponent } from './dynamic-forms/dynamic-field/dynamic-field.component';
+import { PanelComponent } from './panels/panel/panel.component';
 
 import { FrameworkConfigService } from '../btfw/services/framework-config.service';
 import { ScreenService } from '../btfw/services/screen.service';
@@ -46,7 +47,8 @@ import { MenuService } from '../btfw/services/menu.service';
     SignInComponent,
     RegisterUserComponent,
     DynamicFormComponent,
-    DynamicFieldComponent
+    DynamicFieldComponent,
+    PanelComponent
   ],
   providers: [
     FrameworkConfigService,
@@ -55,7 +57,8 @@ import { MenuService } from '../btfw/services/menu.service';
   ],
   exports: [
     FrameworkBodyComponent,
-    DynamicFormComponent
+    DynamicFormComponent,
+    PanelComponent
   ]
 })
 export class BtfwModule { }
